fb0d4c9dc364fa46e1a17d04d6fc86c5315f217c
Allison Nunes <allison.julio@hotmail.com>
2020-04-24 23:38:34 -0300

    Updates

---
+++ precache-manifest.47989b5fc37f1e63f91e4a57785c0bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7982bec7183bb55a8695fcb7c0430792",
+    "url": "/TesteFrontEndTake/index.html"
+  },
+  {
+    "revision": "fce51d7103f7a110aee9",
+    "url": "/TesteFrontEndTake/static/js/2.13269e7f.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/TesteFrontEndTake/static/js/2.13269e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf98df126863b875a2ce",
+    "url": "/TesteFrontEndTake/static/js/main.d9659644.chunk.js"
+  },
+  {
+    "revision": "69672be915e8c1061d9e",
+    "url": "/TesteFrontEndTake/static/js/runtime-main.49109281.js"
+  }
+]);
