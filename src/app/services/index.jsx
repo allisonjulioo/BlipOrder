f9740ff2fa8347e656f2aed386b84ad7869fe2f2
Allison Nunes <32226463+allisonjulioo@users.noreply.github.com>
2023-01-06 08:19:04 -0300

    Update index.jsx

--- src/app/services/index.jsx
+++ src/app/services/index.jsx
@@ -1,2 +1,1 @@
-// export const apiUrl = "https://api-generic-alink.herokuapp.com";
-export const apiUrl = "https://63b8018d6f4d5660c6c9119a.mockapi.io"
+export const apiUrl = "https://63b8018d6f4d5660c6c9119a.mockapi.io/api"
